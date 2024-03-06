9ed4022099d5b35892fdd1255fae3142d2e1dac8
Flibits <116410101+Flibits@users.noreply.github.com>
2024-03-06 18:18:31 -0300

    Update sortearNumero.js

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -6,7 +6,7 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * maiorValor + 1)
 }
 
-console.log('Número secreto:', numeroSecreto)
+console.log('Secret number:', numeroSecreto)
 
 const elementoMenorValor = document.getElementById('menor-valor')
 elementoMenorValor.innerHTML = menorValor
